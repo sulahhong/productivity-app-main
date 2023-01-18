8da92b2ccdd144b8e965196836e940a0f3f2dc21
sulahhong <100662448+sulahhong@users.noreply.github.com>
2023-01-18 21:47:44 +0900

    navbar

--- frontend/src/components/NavbarTop.js
+++ frontend/src/components/NavbarTop.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useReducer, useRef } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "./NavbarTop.module.css";
+import { MdMenu } from "react-icons/md";
+import { AiOutlineMenu } from "react-icons/ai";
 
 function NavbarTop() {
+
+
   return (
-    <div className={styles.navbarTopContainer}>NavbarTop</div>
+    <div className={styles.navbarTopContainer}>
+      <div className={styles.navbarTopLeft} ><AiOutlineMenu size={20} /></div>
+      <div></div>
+    </div>
   )
 }
 
