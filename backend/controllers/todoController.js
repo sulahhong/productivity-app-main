56913fcf7b46d8f8740e1b654c48b83cf8c77bda
sulah <100662448+sulahhong@users.noreply.github.com>
2023-09-13 13:43:28 +0900

    Initial REST API for Todos

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,8 +1,10 @@
 const asyncHandler = require('express-async-handler')
 
+const Todo = require('../model/todoModel')
 
 const getTodos =  asyncHandler(async (req, res) => {
-    res.status(200).json({message : 'Get Todos'})
+    const todos = await Todo.find()
+    res.status(200).json(todos)
 })
 
 const setTodo = asyncHandler(async (req, res) => {
@@ -10,15 +12,39 @@ const setTodo = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('please add a text field')
     }
-    res.status(200).json({message : 'Set Todo'})
+    const todo = await Todo.create({
+        text: req.body.text,
+    })
+
+    res.status(200).json(todo)
 })
 
 const updateTodo = asyncHandler(async (req, res) => {
-    res.status(200).json({message : `Update Todo ${req.params.id}`})
+    const todo = await Todo.findById(req.params.id)
+
+    if (!todo) {
+        res.status(400)
+        throw new Error('Todo not Found')
+    }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, 
+    })
+
+    res.status(200).json(updatedTodo)
 })
 
 const deleteTodo = asyncHandler(async (req, res) => {
-    res.status(200).json({message : `Delete Todo ${req.params.id}`})
+    const todo = await Todo.findById(req.params.id)
+
+    if (!todo) {
+        res.status(400)
+        throw new Error('Todo not Found')
+    }
+
+    await Todo.remove()
+
+    res.status(200).json({ id: req.params.id})
 })
 
 module.exports = {
